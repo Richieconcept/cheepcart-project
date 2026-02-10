6abab24b21d656955b56d14cad63bdac512c177c
[200~Richieconceptgit config --global user.name Richieconceptgit config --global user.name [200~Richieconcept <richieconcept01@gmail.com>
2026-02-10 16:38:41 +0100

    mail port

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -5,8 +5,11 @@ export const sendEmail = async ({ to, subject, html }) => {
     service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS, // Gmail App Password
+      pass: process.env.EMAIL_PASS, // App Password
     },
+    tls: {
+      family: 4 // 👈 FORCE IPv4 (VERY IMPORTANT)
+    }
   });
 
   await transporter.sendMail({
